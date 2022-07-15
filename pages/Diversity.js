de3d4d11c0ec100013575c09594ac60785471147
Isaiah Plascencia <60566856+zaived@users.noreply.github.com>
2022-07-15 16:25:52 -0700

    wasd

--- pages/Diversity.js
+++
@@ -1,9 +0,0 @@
-export default function Diversity() {
-  return (
-    <div className="d-container">
-      <div className="d-content">
-        <h1>COMING SOON...</h1>
-      </div>
-    </div>
-  );
-}
